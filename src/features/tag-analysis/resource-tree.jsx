7bee73ee6f42fcd7d1b39945a99dbfc2d0caecb7
Tommy Shea <tommyshea@users.noreply.github.com>
2023-08-07 15:44:32 -0400

    fixed filter fx and added memoization for initial formatting

--- src/features/tag-analysis/resource-tree.jsx
+++ src/features/tag-analysis/resource-tree.jsx
@@ -5,7 +5,7 @@ import {
     // FormOutlined 
 } from '@ant-design/icons';
 import { Tree , Input } from 'antd';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { debounce } from '@utils/debounce';
 import { ComponentSpinner } from '@components/spinners';
 
@@ -14,144 +14,42 @@ export const ResourceTree = ({
     // onHandleSelectedResources,
     onHandleResourceDetails
 }) => {
-    // console.log('resources: ', resources);
     const [loading, setLoading] = useState(true);
-    // const defaultTreeData = formatTreeData(resources);
-    // console.log('defaultTreeData: ', defaultTreeData);
     const [searchValue, setSearchValue] = useState('');
     const [treeData, setTreeData] = useState([]);
-    // console.log('treeData', treeData);
+    
+    const formattedTreeData = useMemo(() => 
+        formatTreeData(resources), [resources])
 
     const { Search } = Input;
 
-    // console.log('rendered ResourceTree:resources: ', resources);
-
     useEffect(() => {
         if(searchValue==='') {
-            const formattedTreeData = formatTreeData(resources)
             setTreeData(formattedTreeData);
         } else {
-            console.log('searchValue: ', searchValue);
-            console.log('treeData: ', treeData);
-            // const newTreeData = updateResourceTree(searchValue)
-            const newTreeData = [{
-                title: treeData[0].title,
-                children: treeData[0].children.filter(r => r.title.includes(searchValue))
-            }]
-            console.log('newTreeData: ', newTreeData);
+            const newTreeData = filterTreeData(formattedTreeData, searchValue);
             setTreeData(newTreeData);
         }
         setLoading(false)
-    }, [searchValue, resources])
+    }, [searchValue, formattedTreeData])
 
     const handleSearchValueChange = (e) => {
         const { value } = e.target;
-        console.log('typed: ', value);
         setSearchValue(value);
-        // const
     }
 
     const debouncedHandleSearchValueChange = 
         debounce(handleSearchValueChange, 500);
 
-    // const debouncedHandleSearchValueChange = useCallback(() => {
-    //     console.log('debounced search value...');
-    //     return debounce(handleSearchValueChange)
-    // }, [])
-
-    // const updateResourceTree = (searchTerm) => {
-    //     console.log('updateResourceTree:searchTerm: ', searchTerm);
-    //     console.log('updateResourceTree:treeData: ', treeData);
-    //     if(!searchTerm) return treeData
-    //     // const { parentName, icon, children } = treeData
-    //     console.log('updateResourceTree: ', treeData);
-    //     const newTreeData = {
-    //         ...treeData,
-    //         children: treeData.title.children.map(r => {
-    //             return {
-    //                 title: `${r.resourceName} (${r.resourceType})`,
-    //                 key: r.resourceId,
-    //                 icon: <CarryOutOutlined />,
-    //             }
-    //         })
-    //     }
-    //     return newTreeData;
-    // }
-
-    if(!loading && (treeData.length === 0)) {
-        console.log('inside !loading && (treeData.length === 0)')
+    if(!loading && (treeData[0].key === "EMPTY")) {
         return <div>Select a cell to view resource data.</div>
     }
 
-    // if(Object.keys(resources).length === 0) {
-    //     return <div>Select a cell to view resource data.</div>
-    // }
-
-    // const parentName = Object.keys(resources)[0]
-    console.log('treeData: ', treeData);
-    // console.log('treeData:title: ', treeData.title)
-    // console.log('treeData:children: ', treeData.children)
-
-    if(!loading && (treeData.length > 0) && treeData[0].children.length === 0) {
-        return (
-            <>
-                <div>{treeData[0].title}</div>
-                <div className='font-light'>(No Missing Tags)</div>
-            </>
-        )
-    }
-
-    // const defaultTreeData = [
-    //     {
-    //         title: parentName,
-    //         key: parentName,
-    //         icon: <CarryOutOutlined />,
-    //         children: resources[parentName].map(r => {
-    //             return {
-    //                 title: `${r.resourceName} (${r.resourceType})`,
-    //                 key: r.resourceId,
-    //                 icon: <CarryOutOutlined />,
-    //             }
-    //         })
-    //     }
-    // ]
-
-    // const searchTreeData = [
-    //         {
-    //             title: parentName,
-    //             key: parentName,
-    //             icon: <CarryOutOutlined />,
-    //             children: resources[parentName].map(r => {
-    //                 if(r.resourceName.includes(searchValue)) {
-    //                     console.log('match: ', r.resourceName, '=', searchValue)
-    //                     return {
-    //                         title: `${r.resourceName} (${r.resourceType})`,
-    //                         key: r.resourceId,
-    //                         icon: <CarryOutOutlined />,
-    //                     }
-    //                 } else {
-    //                     console.log('not match: ', r.resourceName, '!=', searchValue)
-    //                 }
-    //             })
-    //         }
-    //     ]
-
-    // console.log('searchTreeData: ', searchTreeData);
-
-    // const treeData1 = [
-    //     {
-    //         title: parentName,
-    //         key: parentName,
-    //         icon: <CarryOutOutlined />,
-    //         children: resources[parentName].map(r => {
-    //             return {
-    //                 title: `${r.resourceName} (${r.resourceType})`,
-    //                 key: r.resourceId,
-    //                 icon: <CarryOutOutlined />,
-    //             }
-    //         })
-    //     }
-    // ]
+    const missingTags = (
+        !loading &&
+        (treeData.length > 0) && 
+        (treeData[0].children.length > 0)
+    )
 
     if(loading) {
         return <ComponentSpinner />
@@ -159,29 +57,48 @@ export const ResourceTree = ({
 
     return (
         <div>
-            <p className='font-semibold'>Selected Resources</p>
             <Search 
                 className='mb-1'
                 placeholder='Search'
                 onChange={debouncedHandleSearchValueChange}
-                // onChange={handleSearchValueChange}
             />
-            <Tree
-                className='bg-inherit text-inherit'
-                showLine={true}
-                showIcon={false}
-                onSelect={(resourceId) => onHandleResourceDetails(treeData[0].title, resourceId[0])}
-                defaultExpandAll={true}
-                autoExpandParent={true}
-                treeData={treeData}
-                multiple={false}
-            />
+
+            {!missingTags && (
+                <>
+                    <div className='text-sm'>{treeData[0].title}</div>
+                    <div className='text-sm font-light'>(No Resources Missing Tags)</div>
+                </>
+            )}
+            {missingTags && (
+                <Tree
+                    className='bg-inherit text-inherit'
+                    showLine={true}
+                    showIcon={false}
+                    onSelect={(resourceId) => onHandleResourceDetails(treeData[0].title, resourceId[0])}
+                    defaultExpandAll={true}
+                    autoExpandParent={true}
+                    treeData={treeData}
+                    multiple={false}
+                />
+            )}
         </div>
     )
 }
+
 ResourceTree.propTypes = {
     resources: PropTypes.object,
     onHandleResourceDetails: PropTypes.func
+}
+
+const filterTreeData = (treeData, searchTerm) => {
+    const {title, key, children} = treeData[0]
+    return [{
+        title: title,
+        key: key,
+        children: children.filter(r => 
+            r.title.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+    }]
 }
 
 const formatTreeData = (resources) => {
@@ -213,13 +130,6 @@ const formatTreeData = (resources) => {
                 key: r.resourceId,
                 icon: <CarryOutOutlined />,
                 type: r.resourceType
-                // children: resources[parentName].map(r => {
-                //     return {
-                //         title: `${r.resourceName} (${r.resourceType})`,
-                //         key: r.resourceId,
-                //         icon: <CarryOutOutlined />,
-                //     }
-                // })
             }
         })
     }]
